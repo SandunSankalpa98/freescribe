b6471702e9979857136ea9a537b58ff3bea7cfad
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-06-28 17:42:32 +0530

    Styled specialBtn

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -10,10 +10,12 @@ function HomePage() {
         Record <span className="text-blue-400">&rarr;</span> Transcribe
         <span className="text-blue-400">&rarr;</span> Translate
       </h3>
-      <button className="flex items-center text-base justify-center gap-4 mx-auto w-72 max-w-full my-4">
+      <button className="flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-xl">
         <p>Record</p>
         <i className="fa-solid fa-microphone"></i>
       </button>
+      <p className="text-base">Or <label className="text-blue cursor-pointer hover:text-blue-600 duration-200">Uplaod <input className="hidden" type="file" accept=".mp3,.wave"/></label> a mp3 file</p>
+      <p className="italic text-slate-500">Free now free forever</p>
     </main>
   );
 }
