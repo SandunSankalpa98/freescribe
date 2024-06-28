987168292a30f43adc46acde7063566b31278776
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-06-28 15:49:22 +0530

    Created Header component and Hompage Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+
+import Header from "./Components/Header";
+import HomePage from "./Components/HomePage";
 
 function App() {
   const [count, setCount] = useState(0);
+
   return (
-    <div className='flex flex-col'>
-      <section>
-        <header>
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        <HomePage/>
 
-        </header>
-        
-        <main>
-
-        </main>
-
-        <footer>
-
-        </footer>
+        <footer>{/* Footer content goes here */}</footer>
       </section>
       <h1>hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <main className="flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20">
+      <h1 className=" font-semibold text-5xl sm:text-6xl md:text-7xl ">
+        Free<span className="text-blue-400 bold">Scribe</span>
+      </h1>
+      <h3 className="font-medium md:text-lg">
+        Record <span className="text-blue-400">&rarr;</span> Transcribe
+        <span className="text-blue-400">&rarr;</span> Translate
+      </h3>
+      <button className="flex items-center text-base justify-center gap-4 mx-auto w-72 max-w-full my-4">
+        <p>Record</p>
+        <i className="fa-solid fa-microphone"></i>
+      </button>
+    </main>
+  );
+}
+
+export default HomePage;
