441b78c6d53a93ff5916465c91a76c41b7398969
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-07-02 23:31:27 +0530

    Added loading styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import HomePage from "./Components/HomePage";
 import FileDisplay from "./Components/FileDisplay";
+import Information from "./Components/Information";
+import Transcribing from "./Components/Transcribing";
+
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
   const isAudioAvailable = file || audioStream;
 
   function handleAudioReset() {
@@ -22,7 +29,11 @@ function App() {
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        {isAudioAvailable ? (
+        {output ? (
+          <Information/>
+        ) : loading ? (
+          <Transcribing/>
+        ) : isAudioAvailable ? (
           <FileDisplay file={file} handleAudioReset={handleAudioReset} audioStream={audioStream}/>
         ) : (
           <HomePage setFile={setFile} setAudioStream={setAudioStream} />
--- src/Components/Transcribing.jsx
+++ src/Components/Transcribing.jsx
@@ -3,17 +3,17 @@ import React from "react";
 function Transcribing(props) {
   const { downloading } = props;
   return (
-    <div className="flex items-center flex-col justify-center gap-10 md:gap-14 py-24">
+    <div className="flex items-center flex-col flex-1 justify-center gap-10 md:gap-14 pb-24 text-center">
       <div className="flex flex-col gap-2">
         <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
           <span className="text-blue-400 bold">Transcribing</span>
-          <p>
-            {!downloading ? "warming up cylinders" : "core cylinders engaged"}
-          </p>
         </h1>
+        <p>
+            {!downloading ? "warming up cylinders" : "core cylinders engaged"}
+        </p>
       </div>
 
-      <div className="flex flex-col gap-2 sm:gap-4 max-w-[500px] mx-auto w-full">
+      <div className="flex flex-col gap-2 sm:gap-4 max-w-[400px] mx-auto w-full">
         {[0, 1, 2].map((val) => {
           return (
             <div
