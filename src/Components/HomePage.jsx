d32614e10294038b593497e2afe89201ca682921
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-07-02 12:35:29 +0530

    defined the useStates,useEffect and useRef for create a recording function

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState,useEffect,useRef} from "react";
 
 function HomePage(props) {
 
   const {setAudioStream, setFile} = props
+  const [recordingStatus, setRecordingStatus] = useState('inactive')
+  const [audioChunks, setAudioChunks] = useState([])
+  const [duration, setduration] = useState(0)
+
+  const mediaRecorder = useRef(null)
+  const mimeType = 'audio/webm'
+
+  
+
   return (
     <main className="flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20">
       <h1 className=" font-semibold text-5xl sm:text-6xl md:text-7xl ">
